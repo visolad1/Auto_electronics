59fbf27ada24d55dab1e360d66ab95cc66afdb44
visolad1 <visolad1@gmail.com>
2023-02-11 16:11:36 +0500

    Changes

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var slider = new SimpleAdaptiveSlider('.slider', {
+        loop: false,
+        autoplay: false,
+        interval: 5000,
+        swipe: true,
+    });
+});
